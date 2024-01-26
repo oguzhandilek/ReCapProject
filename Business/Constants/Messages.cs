055e0cd380787a95a37baa8e49f71acef8622b4a
oguzhandilek <oguzhandilek25@gmail.com>
2024-01-26 22:30:37 +0300

    ISTQB Update

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,12 +8,12 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string BrandAdded = "Marka eklendi.";
-        public static string BrandNameInvalid = "Marka ismi geçersiz";
-        public static string BrandofList = "Markalar listelendi";
-        public static string MaintenanceTime = "Sistem bakıma alındı";
-        public static string BrandofUpdated="Marka güncellendi";
-        public static string CarPriceInValid="Araç için kira bedeli boş olamaz";
-        public static string CarofAdded="Araç eklendi";
+        public static readonly string BrandAdded = "Marka eklendi.";
+        public static readonly string BrandNameInvalid = "Marka ismi geçersiz";
+        public static readonly string BrandofList = "Markalar listelendi";
+        public static readonly string MaintenanceTime = "Sistem bakıma alındı";
+        public static readonly string BrandofUpdated="Marka güncellendi";
+        public static readonly string CarPriceInValid="Araç için kira bedeli boş olamaz";
+        public static readonly string CarofAdded="Araç eklendi";
     }
 }
